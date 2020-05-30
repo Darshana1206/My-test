a43dda88d4c50caf88a41e86242944bf474793d4
Darshana1206 <darshanaravindu9196@gmail.com>
2020-05-31 02:26:45 +0530

    pi file read

--- firstScala.scala
+++ firstScala.scala
@@ -1,15 +1,64 @@
 package new1
 
 import scala.util.control.Breaks._
+import scala.io.Source
 object firstScala {
-  def main(hiru:Array[String])
-  {
-   search("ABCDFABC","ABC")
+  def main(args:Array[String]){
+
+    var w:String= (" ")
+    var t:Char=w.charAt(0)
+    
+    var count:Int=0
+    var j:Int=0
+    var i:Int=0
+    var n:Int=0
+    
+    var newArr = new Array[Char](100000000) 
+    
+   for(line <- Source.fromFile("pi.txt").getLines())
+   {
+    
+     
+     count+=1
+     if(count>21)
+     {
+        var piArr= line.toCharArray()
+        var m=piArr.length
+        if(m>20)
+        {
+          for(i<-0 until m)
+          {
+            if(piArr(i)==t)
+            {
+              for(j<-i until (m-1))
+              {
+                piArr(j)=piArr(j+1)
+              }
+            }
+          }
+          for(i<-0 until 50)
+          {
+            newArr(i+n)=piArr(i)
+            
+          }
+          n=n+50
+        }
+        //println(line)
+     }
+    
+   }
+//    for(i<-0 until 100000000)
+//    {
+//      print(newArr(i))
+//    }
+    search(newArr,"880526")
+    
   }
+
   
-  def search(txt:String,pat:String)
+  def search(txt:Array[Char],pat:String)
   {
-    var m:Int=txt.length()
+    var m:Int=txt.length
     var n:Int=pat.length()
     
    
@@ -19,7 +68,8 @@ object firstScala {
     {
        var j:Int=0
       
-      while(j<n && txt.charAt(i+j)== pat.charAt(j)){
+      while(j<n && txt(i+j)== pat.charAt(j))
+      {
          j=j+1 
        
       
